93322680a8b7ab1f2e45ecc52613acfa9cd9095d
Rdttkab <>
2021-09-09 21:00:43 +0000

    style and reactjs is added

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-// const element = <h1>Hello, world</h1>;
-// ReactDOM.render(element, document.getElementById('root'));
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Button(props) {
+  return <input type={props.type} name="" id="" value={props.value} />;
 }
 
-setInterval(tick, 1000);
+function App() {
+  return (
+    <div className="calc">
+      <header className="">
+        <h3>calc</h3> 
+        
+        <form>
+          <h6>THEME</h6>
+          <div>
+            <label></label>
+          </div>
+        </form>
+      </header>
+      <div className="calc-display">
+        <Button type="text" />
+      </div>
+
+      <div className="calc-input">
+        <Button type="button" value="7" className="" />
+        <Button type="button" value="8" className="" />
+        <Button type="button" value="9" className="" />
+        <Button type="button" value="DEL" className="" />
+        <Button type="button" value="4" className="" />
+        <Button type="button" value="5" className="" />
+        <Button type="button" value="6" className="" />
+        <Button type="button" value="+" className="" />
+        <Button type="button" value="1" className="" />
+        <Button type="button" value="2" className="" />
+        <Button type="button" value="3" className="" />
+        <Button type="button" value="-" className="" />
+        <Button type="button" value="." className="" />
+        <Button type="button" value="0" className="" />
+        <Button type="button" value="/" className="" />
+        <Button type="button" value="*" className="" />
+        <Button type="button" value="RESET" className="" />
+        <Button type="button" value="=" className="" />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
