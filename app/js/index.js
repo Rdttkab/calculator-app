c4a7e590a5e8a653ab72d3772660cf186f1250b9
Rediet Tekola <rdt7992@gmail.com>
2021-10-02 23:57:33 +0300

    Theme toggler structure and style is updated

--- app/js/index.js
+++ app/js/index.js
@@ -20,28 +20,26 @@ function App() {
       <header className="header">
         <h3>calc</h3>
 
-        <form className="header__toggle">
-          <h6>THEME</h6>
-          <fieldset
-            className="toggle"
-            aria-label="theme toggle"
-            role="radiogroup"
-          >
-            <div className="toggle__wrapper">
-              <input type="radio" name="theme" id="dark" />
-              <input type="radio" name="theme" id="system" checked />
-              <input type="radio" name="theme" id="light" />
-              <span aria-hidden="true" className="toggle__background"></span>
-              <span aria-hidden="true" className="toggle__button"></span>
-            </div>
+        <fieldset
+          className="toggle"
+          aria-label="theme toggle"
+          role="radiogroup"
+        >
+          <legend>THEME</legend>
+          <div className="toggle__label">
+            <label htmlFor="theme-one">1</label>
+            <label htmlFor="theme-two">2</label>
+            <label htmlFor="theme-three">3</label>
+          </div>
 
-            <div className="toggle__label">
-              <label for="dark">1</label>
-              <label for="system">2</label>
-              <label for="light">3</label>
-            </div>
-          </fieldset>
-        </form>
+          <div className="toggle__wrapper">
+            <input type="radio" name="theme" id="theme-one" defaultChecked />
+            <input type="radio" name="theme" id="theme-two" />
+            <input type="radio" name="theme" id="theme-three" />
+            <span aria-hidden="true" className="toggle__background"></span>
+            <span aria-hidden="true" className="toggle__button"></span>
+          </div>
+        </fieldset>
       </header>
       <div className="calc-display">
         <Button type="text" />
